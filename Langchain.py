b91d99938296ad8927ea6554834be7431c49fa32
LukaszStrzalkowski94 <lstrzalkowski@outlook.com>
2023-10-30 21:58:08 +0100

    Dodałem lang chain

---
+++ Langchain.py
@@ -0,0 +1,24 @@
+from langchain.prompts import PromptTemplate
+from langchain.chat_models import ChatOpenAI
+from langchain.chains import LLMChain
+
+import os
+
+information = """Jerzy VI (ang. George VI, ur. 14 grudnia 1895 w Sandringham House, zm. 6 lutego 1952 tamże) – król Zjednoczonego Królestwa Wielkiej Brytanii i Irlandii Północnej i dominiów (1936–1952), cesarz Indii (1936–1947), król Indii (1947–1949), król Irlandii (1936–1949). Brat Edwarda VIII i jego następca (po abdykacji Edwarda) oraz ojciec Elżbiety II.
+ """
+
+if __name__ == '__main__':
+    print("hello Lang chain")
+    
+    summary_template = """given the information {information} about a person from I want you to create:
+                        1. a short summary 
+                        2. two interesting facts about them 
+                        """
+
+summary_prompt_template = PromptTemplate(input_variables=['information'], template=summary_template)
+
+llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
+
+chain = LLMChain(llm=llm, prompt = summary_prompt_template)
+
+print(chain.run(information=information))
--- ice_breaker.py
+++
@@ -1,9 +0,0 @@
-from langchain import PromptTemplate
-from langchain.chat_models import ChatOpenAI
-from langchain.chains import LLMChain
-
-import os
-
-if __name__ == '__main__':
-    print("hello Lang chain")
-    print(os.environ['OPENAI_API_KEY'])
